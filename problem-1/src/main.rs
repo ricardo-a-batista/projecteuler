62c679c48e6a2a749ad97c1be4c60cc0e77396dd
Ricardo Batista <iamricardobatista@gmail.com>
2024-12-15 21:55:26 +0000

    feat(problem-1): main solution

--- problem-1/src/main.rs
+++ problem-1/src/main.rs
@@ -1,3 +1,20 @@
+use problem_1::NaturalNumbers;
+
 fn main() {
-    println!("Hello, world!");
+    let natural_numbers = NaturalNumbers::new();
+    let natural_numbers = natural_numbers.set_multiples(vec![3, 5]);
+
+    let mut multiples = vec![];
+
+    for multiple in natural_numbers {
+        if multiple < 1000 {
+            multiples.push(multiple);
+            continue;
+        }
+        break;
+    }
+
+    let sum: u32 = multiples.iter().sum();
+
+    println!("The sum of all multiples of 3 and 5 under 1000 is {}", sum);
 }
